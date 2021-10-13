5c74f7665ac3746de53dbee3281d76cdb7ae9fa4
POXS44 <vzeledon7@gmail.com>
2021-10-13 16:49:20 -0600

    Otra prueba para los encargado

--- src/main/java/com/poxs44/Empleado.java
+++ src/main/java/com/poxs44/Empleado.java
@@ -11,7 +11,7 @@ public class Empleado {
             float salariobruto = 0;
             float salario = 1000;
 
-            if(ventasMes>=1000 || ventasMes<= 1499)
+            if(ventasMes>=1000 || ventasMes<= 1499.99)
             {
                salariobruto=  salario + 100;
             }
@@ -25,6 +25,20 @@ public class Empleado {
         else if (TipoEmpleado==("encargado"))
         {
             float salario = 1500;
+            float stotal;
+            float salariobruto = 0;
+
+
+            if(ventasMes>=1000 || ventasMes<= 1499.99)
+            {
+                salariobruto=  salario + 100;
+            }
+            if(ventasMes >=1500)
+            {
+                salariobruto = salario +200;
+            }
+            stotal = (hExtras *20)+salariobruto;
+            return stotal;
         }
         else if (TipoEmpleado==("null"))
         {
--- src/test/java/com/poxs44/EmpleadoTest.java
+++ src/test/java/com/poxs44/EmpleadoTest.java
@@ -5,9 +5,18 @@ import org.junit.Test;
 
 public class EmpleadoTest {
     @Test
-    public void probarSalario()
+    public void probarSalariovendedor()
     {
-        Empleado ei = new Empleado();
-        Assert.assertEquals(1360.0, ei.calcularSalarioBruto("vendedor",2000,8),0.001);
+        Empleado ev = new Empleado();
+        Assert.assertEquals(1360.0, ev.calcularSalarioBruto("vendedor",2000,8),0.001);
+        Assert.assertEquals(1260.0, ev.calcularSalarioBruto("vendedor",1500,3),0.001);
+        Assert.assertEquals(1100.0, ev.calcularSalarioBruto("vendedor",1499.99f,0),0.001);
+
+    }
+    @Test
+    public void probarSalarioencargado()
+    {
+        Empleado ec = new Empleado();
+        Assert.assertEquals(1760.0, ec.calcularSalarioBruto("encargado",1250,8),0.001);
     }
 }
