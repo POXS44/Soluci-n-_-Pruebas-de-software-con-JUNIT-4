3afe2f066bd7aa3433732cf1b6ce2e3468b1e9fe
POXS44 <vzeledon7@gmail.com>
2021-10-13 14:40:10 -0600

    Inicializacion del proyecto de empresa para calcular el sueldo de un empleado

---
+++ src/main/java/com/poxs44/App.java
@@ -0,0 +1,13 @@
+package com.poxs44;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Empleado emp = new Empleado();
+        System.out.println("suma" + emp.calcularSalarioBruto(5,4)
+}
---
+++ src/main/java/com/poxs44/Empleado.java
@@ -0,0 +1,13 @@
+package com.poxs44;
+
+
+
+
+public class Empleado {
+
+   public float calcularSalarioBruto ( float ventasMes, float hExtras)
+    {
+        return ventasMes * hExtras;
+    }
+
+}
