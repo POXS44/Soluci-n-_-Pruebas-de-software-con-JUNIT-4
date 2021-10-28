399d38ddcecf8d10aecc622965b22daf38f0abb6
POXS44 <vzeledon7@gmail.com>
2021-10-27 20:50:09 -0600

    Añadi una condicional mas para decir que si las ventas eran menor que mil dolares entonces me retornara solo el salario base mas la suma de las horas extras

--- src/main/java/com/poxs44/Empleado.java
+++ src/main/java/com/poxs44/Empleado.java
@@ -7,11 +7,11 @@ public class Empleado {
     {
         if(TipoEmpleado==("vendedor"))
         {
-            float stotal;
+            float stotal ;
             float salariobruto = 0;
             float salario = 1000;
 
-            if(ventasMes>=1000 || ventasMes<= 1499.99)
+            if(ventasMes>=1000 || ventasMes<= 1499)
             {
                salariobruto=  salario + 100;
             }
@@ -25,24 +25,25 @@ public class Empleado {
         else if (TipoEmpleado==("encargado"))
         {
             float salario = 1500;
-            float stotal;
+            float stotal ;
             float salariobruto = 0;
 
-
-            if(ventasMes>=1000 || ventasMes<= 1499.99)
+            if (ventasMes <1000){
+               float menos;
+                menos = (hExtras *20)+salariobruto;
+                return menos + salario;
+            }
+            if(ventasMes>=1000 || ventasMes<= 1499)
             {
-                salariobruto=  salario + 100;
+                salariobruto= salario + 100;
             }
             if(ventasMes >=1500)
             {
                 salariobruto = salario +200;
             }
+
             stotal = (hExtras *20)+salariobruto;
             return stotal;
-        }
-        else if (TipoEmpleado==("null"))
-        {
-
         }
 
         return 0;
